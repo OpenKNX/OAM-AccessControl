674c4311f4b8b540234163f602ee8679df3bc9f9
Andreas Breitschopp <a-breitschopp@ab-tools.com>
2024-07-08 22:39:58 +0200

    Updated SwitchActuatorModule.

--- include/versions.h
+++ include/versions.h
@@ -1,10 +1,10 @@
 #pragma once
 
-#define MAIN_Version "bb186fb"
+#define MAIN_Version "e354f74"
 #define KNX_Version "acf4a0b"
 #define MODULE_Common_Version "1.2.0+2d41362"
 #define MODULE_Common_ETS 18
-#define MODULE_SwitchActuator_Version "0.1.0+0143773"
+#define MODULE_SwitchActuator_Version "0.1.0+20b22ce"
 #define MODULE_SwitchActuator_ETS 1
 #define MODULE_LogicModule_Version "3.2.0+eafb950"
 #define MODULE_LogicModule_ETS 50
