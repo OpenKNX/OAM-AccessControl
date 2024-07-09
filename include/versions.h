8f0e8a6933ac2e8577c868de30959fff0d4c034e
Waldemar Porscha <wp@porscha.eu>
2024-07-09 17:45:08 +0200

    Release preparation 0.3:
    - Align "Allgemein" with new version display
    - Align release builds with dev builds
    - prepare ETS-App for secure (inaktive)
    - Bump version to 0.3

--- include/versions.h
+++ include/versions.h
@@ -1,12 +1,12 @@
 #pragma once
 
-#define MAIN_Version "674c431"
+#define MAIN_Version "55ca183"
 #define KNX_Version "e69f636"
-#define MODULE_Common_Version "1.2.0+e9a0a5b"
+#define MODULE_Common_Version "1.2.0+73e09e3"
 #define MODULE_Common_ETS 18
 #define MODULE_SwitchActuator_Version "0.1.0+19138d9"
 #define MODULE_SwitchActuator_ETS 1
-#define MODULE_LogicModule_Version "3.3.0+bcf8371"
+#define MODULE_LogicModule_Version "3.3.0+73b252b"
 #define MODULE_LogicModule_ETS 51
 #define MODULE_BinaryInput_Version "0.2.0+9fd5ad1"
 #define MODULE_BinaryInput_ETS 2
