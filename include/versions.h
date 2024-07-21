c917860a795cd389a6d8b99b389ead05e34a4290
Andreas Breitschopp <a-breitschopp@ab-tools.com>
2024-07-21 20:16:59 +0200

    Release 0.6.1.

--- include/versions.h
+++ include/versions.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define MAIN_Version "cbb2d44"
+#define MAIN_Version "2eb6cf0"
 #define KNX_Version "2.1.0+e69f636"
 #define MODULE_Common_Version "1.2.0+ddd1363"
 #define MODULE_Common_ETS 18
