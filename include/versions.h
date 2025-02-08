e292d01b3d5be52a9c0a7fb5e65892a1c8d2eb49
Andreas Breitschopp <a-breitschopp@ab-tools.com>
2025-02-08 10:53:58 +0100

    Added GPIOModule for compatibility with SwitchActuatorModule.

--- include/versions.h
+++ include/versions.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define MAIN_Version "7dc62be"
+#define MAIN_Version "aa4a127"
 #define KNX_Version "2.1.2+764900e"
 #define MODULE_Common_Version "1.2.0+6f467fb"
 #define MODULE_Common_Version_Major 1
@@ -12,6 +12,11 @@
 #define MODULE_SwitchActuator_Version_Minor 1
 #define MODULE_SwitchActuator_Version_Revision 0
 #define MODULE_SwitchActuator_ETS 1
+#define MODULE_GPIOModule_Version "0.0.0+39b2592"
+#define MODULE_GPIOModule_Version_Major 0
+#define MODULE_GPIOModule_Version_Minor 0
+#define MODULE_GPIOModule_Version_Revision 0
+#define MODULE_GPIOModule_ETS 0
 #define MODULE_LogicModule_Version "3.5.2+9a1cfe1"
 #define MODULE_LogicModule_Version_Major 3
 #define MODULE_LogicModule_Version_Minor 5
